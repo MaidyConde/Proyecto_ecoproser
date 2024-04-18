6210a60900674283bd2eb147c0c4407487a53bbd
Maidy Conde <maidyladino04@gmail.com>
2024-04-18 12:32:12 -0500

    yssysgxy

---
+++ ecoproser/src/main/java/ecoproser/ecoproser/EcoproserApplication.java
@@ -0,0 +1,13 @@
+package ecoproser.ecoproser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoproserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoproserApplication.class, args);
+	}
+
+}
---
+++ ecoproser/src/test/java/ecoproser/ecoproser/EcoproserApplicationTests.java
@@ -0,0 +1,13 @@
+package ecoproser.ecoproser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoproserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
