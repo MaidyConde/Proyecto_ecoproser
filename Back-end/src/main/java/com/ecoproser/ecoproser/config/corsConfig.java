7e26fe242e6446b3d68c7282e32934036e28b052
Maidy Conde <maidyladino04@gmail.com>
2024-04-18 17:47:20 -0500

    listo el config

---
+++ Back-end/src/main/java/com/ecoproser/ecoproser/config/corsConfig.java
@@ -0,0 +1,35 @@
+package com.ecoproser.ecoproser.config;
+
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class corsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        //permitir solicitudes desde todos los origenes
+        //config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://127.0.0.1:5500/");
+
+        //permitir solicitudes con estos metodos HTTP
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        //permitir el envio de ciertos encabezados en las solicitudes
+        config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("Content-Type");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
